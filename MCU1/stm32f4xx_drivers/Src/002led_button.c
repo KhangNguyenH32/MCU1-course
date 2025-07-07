5c2353daabb1219ad0cb3948da989f24e29ce52d
Nguyen Khang <goatold67@gmail.com>
2025-07-07 20:24:21 +0700

    exercise: button input controls LED toggle

---
+++ MCU1/stm32f4xx_drivers/Src/002led_button.c
@@ -0,0 +1,47 @@
+/*
+ * 002led_button.c
+ *
+ *  Created on: Jul 7, 2025
+ *      Author: goato
+ */
+
+#include"stm32f407xx.h"
+
+void delay(void)
+{
+	for(uint32_t i = 0; i < 500000; i++);
+}
+
+
+int main(void)
+{
+	GPIO_handle_t gpioLED, gpioButton;
+
+	gpioLED.pGPIOx = GPIOD;
+	gpioLED.GPIO_PinConfig.GPIO_PinNumber = GPIO_PIN_NO_12;
+	gpioLED.GPIO_PinConfig.GPIO_PinMode = GPIO_MODE_OUT;
+	gpioLED.GPIO_PinConfig.GPIO_PinSpeed = GPIO_SPEED_FAST;
+	gpioLED.GPIO_PinConfig.GPIO_PinOPType = GPIO_OP_TYPE_OD;
+	gpioLED.GPIO_PinConfig.GPIO_PinPuPdControl = GPIO_PIN_PU;
+
+	gpioButton.pGPIOx = GPIOA;
+	gpioButton.GPIO_PinConfig.GPIO_PinNumber = GPIO_PIN_NO_0;
+	gpioButton.GPIO_PinConfig.GPIO_PinMode = GPIO_MODE_IN;
+	gpioButton.GPIO_PinConfig.GPIO_PinSpeed = GPIO_SPEED_FAST;
+	gpioButton.GPIO_PinConfig.GPIO_PinPuPdControl = GPIO_NO_PUPD;
+
+	GPIO_PeriClockControl(GPIOA, ENABLE);
+	GPIO_PeriClockControl(GPIOD, ENABLE);
+
+	GPIO_Init(&gpioLED);
+	GPIO_Init(&gpioButton);
+
+	while(1)
+	{
+		if(GPIO_ReadFromInputPin(GPIOA, GPIO_PIN_NO_0))
+		{
+			//delay();
+			GPIO_ToggleOutputPin(GPIOD, GPIO_PIN_NO_12);
+		}
+	}
+}
